c8687971e6eecc28c2e02f5ce41c6342f23efaea
youjeongPark <pyj0695@gmail.com>
2021-08-09 23:27:15 +0900

    MongoDB Cluster Connect

--- server/app.js
+++ server/app.js
@@ -1,6 +1,15 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import config from './config'
 
 const app = express()
+const {MONGO_URI} = config
+
+mongoose.connet(MONGO_URI,{
+    useNewUrlParser : true
+})
+.then(()=> console.log("MongoDB conncting Success!! "))
+.catch((e) => console.log(e))
 
 app.get('/')
 
